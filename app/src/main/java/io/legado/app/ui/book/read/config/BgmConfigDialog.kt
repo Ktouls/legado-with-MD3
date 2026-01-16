0e57466e3df2693cda3c61181eb813178d0ef255
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-17 01:33:55 +0800

    Create BgmConfigDialog.kt

---
+++ app/src/main/java/io/legado/app/ui/book/read/config/BgmConfigDialog.kt
@@ -0,0 +1,81 @@
+package io.legado.app.ui.book.read.config
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import io.legado.app.R
+import io.legado.app.base.BaseDialogFragment
+import io.legado.app.databinding.DialogBgmConfigBinding
+import io.legado.app.help.config.AppConfig
+import io.legado.app.service.BgmManager
+import io.legado.app.ui.file.HandleFileContract
+import io.legado.app.utils.setLayout
+import io.legado.app.utils.viewbindingdelegate.viewBinding
+
+class BgmConfigDialog : BaseDialogFragment(R.layout.dialog_bgm_config) {
+
+    private val binding by viewBinding(DialogBgmConfigBinding::bind)
+
+    // 文件夹选择器
+    private val selectDir = registerForActivityResult(HandleFileContract()) {
+        it.uri?.let { uri ->
+            AppConfig.bgmUri = uri.toString()
+            binding.tvPath.text = uri.path
+            BgmManager.loadBgmFiles()
+            BgmManager.play()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setLayout(0.9f, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initData()
+    }
+
+    private fun initData() = binding.run {
+        switchBgm.isChecked = AppConfig.isBgmEnabled
+        tvPath.text = AppConfig.bgmUri.ifEmpty { "未选择文件夹" }
+        // Slider 需要设置 Float 类型的值
+        seekVolume.value = AppConfig.bgmVolume.toFloat()
+        tvVolumeValue.text = "${AppConfig.bgmVolume}%"
+    }
+
+    private fun initView() = binding.run {
+        switchBgm.setOnCheckedChangeListener { _, isChecked ->
+            AppConfig.isBgmEnabled = isChecked
+            if (isChecked) BgmManager.play() else BgmManager.pause()
+        }
+
+        btnSelectFolder.setOnClickListener {
+            selectDir.launch {
+                mode = HandleFileContract.DIR
+            }
+        }
+
+        // 适配 Material3 Slider 的监听器 (替代了之前的 SeekBar 监听)
+        seekVolume.addOnChangeListener { _, value, fromUser ->
+            if (fromUser) {
+                val progress = value.toInt()
+                BgmManager.setVolume(progress)
+                tvVolumeValue.text = "$progress%"
+            }
+        }
+
+        btnPrev.setOnClickListener { BgmManager.prev() }
+        btnNext.setOnClickListener { BgmManager.next() }
+        btnPlayPause.setOnClickListener {
+            if (BgmManager.isPlaying()) {
+                BgmManager.pause()
+                btnPlayPause.text = "播放"
+            } else {
+                BgmManager.play()
+                btnPlayPause.text = "暂停"
+            }
+        }
+    }
+}
