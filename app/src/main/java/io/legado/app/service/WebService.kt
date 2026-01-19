f1861be1a9a0b6f7659db6a516cde0acb2c63dea
Ktouls <75007654+Ktouls@users.noreply.github.com>
2026-01-19 18:11:56 +0800

    Update WebService.kt

--- app/src/main/java/io/legado/app/service/WebService.kt
+++ app/src/main/java/io/legado/app/service/WebService.kt
@@ -3,6 +3,7 @@ package io.legado.app.service
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.PowerManager
 import androidx.core.app.NotificationCompat
 import io.legado.app.R
